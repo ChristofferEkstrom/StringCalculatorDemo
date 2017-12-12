386f4f0b4096c74befc44690c0db4a161c0c8de4
Christoffer Ekström <31304101+ChristofferEkstrom@users.noreply.github.com>
2017-12-12 15:06:53 +0100

    Add project files.

---
+++ StringCalculatorDemo.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StringCalculatorDemo.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StringCalculatorDemo.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3fdd6e6c-c337-4fe5-a6a1-7f8896b78f26")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ StringCalculatorDemo/StringCalculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculatorDemo
+{
+    public class Calculator
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="expression"></param>
+        /// <returns></returns>
+        public int Add(string expression)
+        {
+            if (string.IsNullOrEmpty(expression))
+                return 0;
+
+            int sum = 0;
+            if (expression.Contains(","))
+            {
+                string[] digits = expression.Split(',');
+                foreach(string digit in digits)
+                {
+                    sum += int.Parse(digit);
+                }
+                //sum = int.Parse(digits[0]) + int.Parse(digits[1]);
+
+            }
+            else
+            {
+                sum = int.Parse(expression);
+            }
+            return sum;
+
+            //return int.Parse(expression);
+        }
+    }
+}
+
